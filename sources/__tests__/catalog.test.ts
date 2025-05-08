14022f384895ab6e77111207c839fa9a391a6cd3
이호연 <ho991217@kakao.com>
2025-05-08 15:11:12 +0900

    fix: fix conflict with patched deps (#11)

--- sources/__tests__/catalog.test.ts
+++ sources/__tests__/catalog.test.ts
@@ -595,4 +595,32 @@ describe("yarn-plugin-catalogs", () => {
 
     await expect(workspace.yarn(['dlx', '@jwoo0122/echo', '"hello, world"'])).resolves.not.toThrow();
   });
+
+  it("should resolve patched dependency", async () => {
+    workspace = await createTestWorkspace();
+
+    await workspace.writeYarnrc({
+      catalogs: {
+        list: {
+          /**
+           * @context test issue with the version specified in [issue#10](https://github.com/toss/yarn-plugin-catalogs/issues/10)
+           */
+          typescript: "5.8.3",
+        },
+      },
+    });
+
+    await workspace.yarn.add("typescript");
+
+    const { stdout: listOutput } = await workspace.yarn.info();
+    const dependencies = extractDependencies(listOutput);
+
+    expect(
+      dependencies.some((dep) =>
+        /typescript@patch:typescript@npm%3A5\.8\.3#optional!builtin<compat\/typescript>::version=5\.8\.3&hash=[a-f0-9]+/.test(
+          dep
+        )
+      )
+    ).toBe(true);
+  });
 });
