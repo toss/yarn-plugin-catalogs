7ebb0525aeca9d8e54147276e78364786c06ccd8
parksb <parkgds@gmail.com>
2025-06-27 14:37:02 +0900

    add test cases for validation with default option

--- sources/__tests__/catalog.test.ts
+++ sources/__tests__/catalog.test.ts
@@ -828,4 +828,61 @@ describe("yarn-plugin-catalogs", () => {
     expect(stderr).toContain("react");
     expect(stderr).toContain("react@catalog:");
   });
+
+  it("should use default alias group without validation warning", async () => {
+    workspace = await createTestWorkspace();
+
+    await workspace.writeYarnrc({
+      catalogs: {
+        options: {
+          validation: "warn",
+          default: ["stable"],
+        },
+        list: {
+          stable: {
+            react: "npm:17.0.0",
+          },
+          beta: {
+            react: "npm:18.0.0",
+          },
+        },
+      },
+    });
+
+    const { stdout, stderr } = await workspace.yarn.add("react");
+    expect(stdout).not.toContain("catalog");
+    expect(stderr).toBe("");
+
+    const { stdout: listOutput } = await workspace.yarn.info();
+    const dependencies = extractDependencies(listOutput);
+    expect(dependencies).includes("react@npm:17.0.0");
+  });
+
+  it("should use default alias group without validation error", async () => {
+    workspace = await createTestWorkspace();
+
+    await workspace.writeYarnrc({
+      catalogs: {
+        options: {
+          validation: "strict",
+          default: ["stable"],
+        },
+        list: {
+          stable: {
+            react: "npm:17.0.0",
+          },
+          beta: {
+            react: "npm:18.0.0",
+          },
+        },
+      },
+    });
+
+    const { stderr } = await workspace.yarn.add("react");
+    expect(stderr).toBe("");
+
+    const { stdout: listOutput } = await workspace.yarn.info();
+    const dependencies = extractDependencies(listOutput);
+    expect(dependencies).includes("react@npm:17.0.0");
+  });
 });
